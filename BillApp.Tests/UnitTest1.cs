b12a4a5c9c12a7ab2e501683b949abf4a183e426
Hewitt VS <hewittvs@gmail.com>
2019-06-28 00:04:40 +0530

    Included VB project into code coverage

--- BillApp.Tests/UnitTest1.cs
+++ BillApp.Tests/UnitTest1.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using BillApp.BusinessLogic;
+using MRStudio_new;
 
 namespace BillApp.Tests
 {
@@ -12,6 +13,12 @@ namespace BillApp.Tests
         {
             Adder adder = new Adder();
             Assert.AreEqual(4, adder.Add(2,2));
+        }
+
+        public void Test2()
+        {
+            Subtract sub = new Subtract();
+            Assert.AreEqual(4, sub.Subtract(8, 4));
         }
     }
 }
---
+++ MRStudio_new/Class1.vb
@@ -0,0 +1,7 @@
+﻿Public Class Subtract
+    Public Function Subtract(a As Integer, b As Integer)
+        'TODO: This line of code loads data into the 'MRStudioDataSet1.ProductDetail' table. You can move, or remove it, as needed.
+        Return a - b
+    End Function
+
+End Class
