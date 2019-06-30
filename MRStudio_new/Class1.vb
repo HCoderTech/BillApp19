550bfc8c46c5c981d2cb8599b9b12a29c54b67c8
Hewitt VS <hewittvs@gmail.com>
2019-06-30 13:03:14 +0530

    Removed todo in subtract

--- MRStudio_new/Class1.vb
+++ MRStudio_new/Class1.vb
@@ -1,6 +1,5 @@
 ﻿Public Class Subtract
     Public Function Subtract(a As Integer, b As Integer)
-        'TODO: This line of code loads data into the 'MRStudioDataSet1.ProductDetail' table. You can move, or remove it, as needed.
         Return a - b
     End Function
 
