6bbe9d50558bd81b6adad5c326f3d703fab6a94f
Hewitt VS <hewittvs@gmail.com>
2019-07-04 01:46:13 +0530

    Include MRStudio for code coverage

---
+++ BillApp.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using MRStudio_new;
+using NUnit.Framework;
+
+namespace BillApp.Tests
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            Subtract sub = new Subtract();
+            Assert.AreEqual(4, sub.Subtract(8, 4));
+        }
+    }
+}
